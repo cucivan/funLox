a5ecc988caa1c2b4c5c36beabef33a3c8fa4ded2
cucivan <151307667+cucivan@users.noreply.github.com>
2024-12-30 22:01:54 +0800

    first day

---
+++ src/jlox/Lox.java
@@ -0,0 +1,70 @@
+package jlox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Lox {
+    //field
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+    public static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        //for indicate a error happen in the excuction of code
+        if (hadError)
+            System.exit(65);
+    }
+
+    public static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        while(true) {
+            System.out.println("eviljlox>");
+
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            // some error happen when running code
+
+            //reset the value of hadError, ensure when a new cmd come, everything be ok
+            hadError = false;
+        }
+    }
+    public static void run(String source) {
+        Scanner scaner = new Scanner(source);
+        List<Token> tokens = scaner.scanTokens();
+
+        // just print tokens
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void err(int line, String message) {
+        report(line, "", message);
+    }
+    private static void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+
+}
